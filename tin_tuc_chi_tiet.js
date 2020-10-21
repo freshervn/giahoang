bec8df70742b91c96fec1af7560acbf21324d257
freshervn <40496285+freshervn@users.noreply.github.com>
2020-10-21 17:25:38 +0700

    oke up

--- tin_tuc_chi_tiet.js
+++ tin_tuc_chi_tiet.js
@@ -1,42 +1,73 @@
+
 $('.related-products-carousel').owlCarousel({
-				loop:true,
-				margin:10,
-				nav:true,
-				dots:false,
-				responsive:{
-					0:{
-						items:1
-					},
-					586:{
-						items:2
-					},
-					768:{
-						items:3
-					},
-					992:{
-						items:4
-					}
-				}
-			})
+	loop:true,
+	margin:10,
+	nav:true,
+	dots:false,
+	responsive:{
+		0:{
+			items:1
+		},
+		586:{
+			items:2
+		},
+		768:{
+			items:3
+		},
+		992:{
+			items:4
+		}
+	}
+})
 $('.related-news-carousel').owlCarousel({
-				loop:true,
-				margin:10,
-				nav:false,
-				dots:true,
-				responsive:{
-					0:{
-						items:1
-					},
-					586:{
-						items:2
-					},
-					768:{
-						items:3
-					},
-					992:{
-						items:3
-					}
-				}
-			})
+	loop:true,
+	margin:10,
+	nav:true,
+	dots:true,
+	responsive:{
+		0:{
+			items:1
+		},
+		586:{
+			items:2
+		},
+		768:{
+			items:3
+		},
+		992:{
+			items:3
+		}
+	}
+})
 
+// document.addEventListener("load",responsive());
 
+// function responsive() {
+	
+// 	let q= document.matchMedia("(max-with:576px)");
+// alert("hi");	
+// 	if(query.matches)
+// 	{
+
+// 		// $('.related-news-carousel').owlCarousel({
+// 		// 	loop:true,
+// 		// 	margin:10,
+// 		// 	nav:false,
+// 		// 	dots:false,
+// 		// 	responsive:{
+// 		// 		0:{
+// 		// 			items:1
+// 		// 		},
+// 		// 		586:{
+// 		// 			items:2
+// 		// 		},
+// 		// 		768:{
+// 		// 			items:3
+// 		// 		},
+// 		// 		992:{
+// 		// 			items:3
+// 		// 		}
+// 		// 	}
+// 		// })		
+// 	}		
+// }
